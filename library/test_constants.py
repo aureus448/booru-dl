3ccf7aa2d672ed5f6e3a3622a005186f5f9e6d7e
Nate Ruppert <nathaniel.ruppert@sce.com>
2021-06-08 00:08:22 -0700

    updated tests for get_uri

--- library/test_constants.py
+++ library/test_constants.py
@@ -1,17 +1,23 @@
 import os
-from unittest import TestCase
+
+import pytest
 
 from library import constants
 
 
-class get_uri(TestCase):
-    def test_get_uri(self):
-        # Make uri.ini and make sure stuff exists
-        if not os.path.exists("uri.ini"):
-            file = open("uri.ini", "w+")
-            file.write("[URI]\n")
-            file.write("#Put URI Here\n")
-            file.write("uri=apple.net\n")
-            file.close()
-        config = constants.get_uri()
-        assert type(config) == str
+def test_get_uri():
+    # Ensures there is a uri.ini and make sure stuff exists
+    if not os.path.exists("uri.ini"):
+        file = open("uri.ini", "w+")
+        file.write("[URI]\n")
+        file.write("#Put URI Here\n")
+        file.write("uri=apple.net\n")
+        file.close()
+    config = constants.get_uri()
+    assert type(config) == str
+
+
+def test_get_uri_fail():
+    # Blank pass-through of config value will fail
+    with pytest.raises(ValueError):
+        constants.get_uri("")

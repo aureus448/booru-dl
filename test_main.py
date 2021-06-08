c30b4df535889fb5f64d3e318f05a245c2992cac
Nate Ruppert <nathaniel.ruppert@sce.com>
2021-06-08 14:28:40 -0700

    debug of actions still unknown issue

--- test_main.py
+++ test_main.py
@@ -13,12 +13,15 @@ def make_uri():
     path = os.path.abspath("library/uri.ini")
     print(f"Path for Make URI: {path}")
     if not os.path.exists(path):
+        print("Made file")
         file = open(path, "w+")
         file.write("[URI]\n")
         file.write("#Put URI Here\n")
         print(f"Environment Variable for URI: {os.environ['URI']}")
         file.write(f"uri={os.environ['URI']}\n")
         file.close()
+        with open(path) as file:
+            print(file.readlines())
 
 
 def test_download_files():
